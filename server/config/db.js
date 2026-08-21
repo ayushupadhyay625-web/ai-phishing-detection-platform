4e2573207fe92b63f947403805b489ccc5b099bb
Ayush Upadhyay <ayushupadhyay625@gmail.com>
2026-08-21 12:30:24 +0530

    Configure MongoDB Atlas database

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,27 @@
 import mongoose from "mongoose";
 
+
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGODB_URI);
+    const connection =
+      await mongoose.connect(
+        process.env.MONGODB_URI,
+        {
+          dbName: "phishing_detection",
+        }
+      );
 
     console.log(
       `MongoDB connected: ${connection.connection.host}/${connection.connection.name}`
     );
   } catch (error) {
-    console.error(`MongoDB connection failed: ${error.message}`);
+    console.error(
+      `MongoDB connection failed: ${error.message}`
+    );
+
     process.exit(1);
   }
 };
+
 
 export default connectDB;
